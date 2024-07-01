289bd8484e4c06408a51d1cf18a0adaee8a0f95a
Usman Tahir <usman.tahir@datumbrain.com>
2024-07-01 17:44:53 +0500

    Bash Task Submission

---
+++ utils.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 -o merge -i <dir> -p <dir> -f csv"
+	exit 1
+}
+
+operation=""
+input_path=""
+output_path=""
+format="json"
+
+while getopts ":o:i:p:f:" opt; do
+
+	case $opt in
+	       	o) operation="$OPTARG" ;;
+   		i) input_path="$OPTARG" ;;
+    		p) output_path="$OPTARG" ;;
+    		f) format="$OPTARG" ;;
+    		\?) echo "Invalid option:"; usage ;;
+ 	 esac
+done
+
+if [[ ! -d "$input_path" ]]; then
+	echo "Input directory '$input_path' does not exist."
+	exit 1
+fi
+
+output_file="$output_path/merged_data.$format"
+
+merged_data=""
+for file in "$input_path"/*.json; do
+	if [[ -f "$file" ]]; then
+		merged_data+=$(cat "$file")$'\n'
+fi
+done
+
+if [[ -z "$merged_data" ]]; then
+	echo " No JSONL files found in the input directory."
+	exit 0
+fi
+
+if [[ "$format" == "csv" ]]; then
+	echo "$merged_data" | jq -r '
+  map(if type == "array" then .[] else . end) |
+  (.[0] | to_entries | map(.key)) as $keys |
+  $keys, map(. as $value | $keys[] as $key | [$key, $value[$key]])[] | @csv
+  ' > "$output_path/output.csv"
+	output_file="$output_path/output.csv"
+else
+	echo "$merged_data" > "$output_file"
+fi
+
+
+echo "Merged data saved to: $output_file"
+
